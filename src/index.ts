a29ce4083dd4afb90f9004bf2f07b4aa9557f942
Evgeny <evgeny.efimenko@proton.me>
2024-05-06 04:52:13 +0500

    feat: eslint configuration

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { configuration } from './configuration'
 
 app.listen({ port: configuration.PORT }, (err, address) => {
   if (err) {
+    // eslint-disable-next-line no-console
     console.error(err)
     process.exit(1)
   }
+  // eslint-disable-next-line no-console
   console.log(`Server listening at ${address}`)
 })
