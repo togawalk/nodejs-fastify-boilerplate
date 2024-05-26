f3b15c26b8f3ec526078c74211fd4a101dd93526
Evgeny <evgeny.efimenko@proton.me>
2024-05-26 16:58:01 +0500

    fix: add INFO message when LOG_TO_TERM=false

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import app from './app'
 import { configuration } from './configuration'
 
 app.listen({ port: configuration.PORT }, (err, address) => {
+  const sucessMessage = `Server listening at ${address}`
   if (err) {
     app.log.error(err)
     process.exit(1)
   }
-  app.log.info(`Server listening at ${address}`)
+  app.log.info(sucessMessage)
+  if (configuration.logger.LOG_TO_TERM === false) {
+    console.info(sucessMessage)
+  }
 })
